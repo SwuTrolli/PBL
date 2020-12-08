d3f2f841ce712452047a9b4f47166525c6c24435
gyuri-kim7641 <70854024+gyuri-kim7641@users.noreply.github.com>
2020-12-08 20:30:30 +0900

    re-filtering specific words

--- swu_Trolli_src/inject.js
+++ swu_Trolli_src/inject.js
@@ -9,10 +9,10 @@ var replaceTextInNode = function(parentNode, list_keywords){
          {
             if (node.textContent.indexOf(list_keywords[idx]) >= 0 )
             {
-// 기존 코
+// 기존 코드 주석처리
 //               node.textContent = Array(node.textContent.length).join("*"); /* modify text here */
             }
-// 특정 단어만 필터
+// 특정 단어만 필터처리
             if (node.textContent.indexOf(list_keywords[idx]) >= 0 )
             {
                node.textContent = replaceAll(node.textContent, list_keywords[idx], Array(list_keywords[idx].length+1).join("*")); /* modify text here */
@@ -28,16 +28,20 @@ var replaceTextInNode = function(parentNode, list_keywords){
     }
 };
 
+function replaceAll(str, searchStr, replaceStr) {
+   return str.split(searchStr).join(replaceStr);
+};
+
 function run_textfree(node) {
-		chrome.storage.sync.get("textfreestorage", function (items) {
+      chrome.storage.sync.get("textfreestorage", function (items) {
 
-			var list_keywords = items["textfreestorage"].list_keywords;
+         var list_keywords = items["textfreestorage"].list_keywords;
 
-			if ( list_keywords !== undefined )
-			{
-				replaceTextInNode(node, list_keywords);
-			}
-		});
+         if ( list_keywords !== undefined )
+         {
+            replaceTextInNode(node, list_keywords);
+         }
+      });
 
 }
 
@@ -45,12 +49,11 @@ function run_textfree(node) {
 run_textfree(document.body);
 
 document.addEventListener("DOMSubtreeModified", function(event){
-	run_textfree(event.target);
+   run_textfree(event.target);
 });
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
      if (request.greeting == "message_textfree") {
-		run_textfree(document.body);
-	 }
+      run_textfree(document.body);
+    }
 });
-
